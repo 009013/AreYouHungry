5174b4704a93f2a1a2764d54179bd9df7850d70d
009013 <504421876@qq.com>
2021-04-19 19:08:11 +0800

    1

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Header from '@/components/Header'
 import './index.less'
 function Login() {
+    const [color, setColor] = useState('span')
+    const [cole, setCloe] = useState('box')
+    const boxFun = e => {
+        if(color === 'span'){
+            setColor('on')
+            setCloe('on1')
+
+        } else {
+           setColor('span')
+           setCloe('box')
+        }
+    }
     return (
         <div className="login">
             <Header/>
+            <div className="loginSection">
+                <div>
+                    <input type="text"/>
+                </div>
+                <div>
+                    <input type="text"/>
+                    <p className={cole} onClick={boxFun}>
+                        <span className={color}></span>
+                    </p>
+                </div>
+                <div>
+                    <p><input type="text" placeholder="验证码"/></p>
+                    <p>
+                        <font>12312</font>
+                        <button>看不清换一张</button>
+                    </p>
+                </div>
+            </div>
+            <div>
+                <p>温馨提示：未注册过的账号，登录时将会自动注册</p>
+                <p>注册过的用户可凭账号密码登录</p>
+            </div>
+            <p>
+                <button>登录</button>
+            </p>
+            <p>重置密码?</p>
         </div>
     )
 }
